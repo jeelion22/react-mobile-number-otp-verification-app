fcfda77b579dcb85c5be229d06d175780c51e1d1
jeeva m <jeelion22@gmail.com>
2024-06-18 10:42:09 +0530

    updated features

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,82 @@ import { faLock, faPhone } from "@fortawesome/free-solid-svg-icons";
 import OtpInput from "react-otp-input";
 import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
+import { auth } from "./firebase.config";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 function App() {
   const [otp, setOtp] = useState("");
-  const [spinner, setSpinner] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [value, setValue] = useState();
+  const [user, setUser] = useState(null);
 
   const [showOtp, setShowOtp] = useState(false);
   const [showPhone, setShowPhone] = useState(true);
+  const [error, setError] = useState("");
 
+  function onCaptchVerify() {
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier(
+        auth,
+        "recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            // ...
+            onSignup();
+          },
+          "expired-callback": () => {
+            // Response expired. Ask user to solve reCAPTCHA again.
+            // ...
+            setError("reCAPTCHA expired. Please try again.");
+          },
+        }
+      );
+    }
+  }
+
+  function onSignup() {
+    setLoading(true);
+    setError("");
+    onCaptchVerify();
+
+    const appVerifier = window.recaptchaVerifier;
+
+    const formatPhone = "+" + value;
+
+    signInWithPhoneNumber(auth, formatPhone, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        setLoading(false);
+        setShowPhone(false);
+        setShowOtp(true);
+      })
+      .catch((error) => {
+        console.log("Error during signInWithPhoneNumber:", error);
+        setError("Failed to send OTP Input. Please try again later");
+        setLoading(false);
+      });
+  }
+
+  function onOTPVerify() {
+    setLoading(true);
+    setError("");
+    window.confirmationResult
+      .confirm(otp)
+      .then(async (res) => {
+        console.log(res.user);
+        setUser(res.user);
+        setLoading(false);
+        setShowOtp(false);
+        setShowPhone(false);
+      })
+      .catch((error) => {
+        console.log("Error during OTP verification:", error);
+        setError("Invalid OTP. Please try again.");
+        setLoading(false);
+      });
+  }
   return (
     <div className="container vh-100">
       <div className="row h-100 justify-content-center align-items-center">
@@ -20,6 +87,12 @@ function App() {
           <h1 className="text-white mt-2 mb-6 ">
             Welcome to <br /> CODE A PROGRAM
           </h1>
+
+          <div id="recaptcha-container"></div>
+
+          {error && <p className="text-danger">{error}</p>}
+
+          {user && <h2 className="text-center text-white ">👍Login Success</h2>}
 
           {showPhone && (
             <>
@@ -41,9 +114,19 @@ function App() {
                   onChange={setValue}
                 />
               </div>
-              <button className="btn btn-primary mb-4 ">
-                Send code via SMS
-              </button>
+              <button
+                className="btn btn-dark "
+                type="button"
+                onClick={onSignup}
+              >
+                {loading && (
+                  <span
+                    className="spinner-grow spinner-grow-sm me-2"
+                    aria-hidden="true"
+                  ></span>
+                )}{" "}
+                <span role="status">Send OTP via SMS</span>
+              </button>{" "}
             </>
           )}
 
@@ -71,10 +154,14 @@ function App() {
                     renderInput={(props) => <input {...props} />}
                   />
                 </div>
-                <button class="btn btn-dark " type="button">
-                  {spinner && (
+                <button
+                  className="btn btn-dark "
+                  type="button"
+                  onClick={onOTPVerify}
+                >
+                  {loading && (
                     <span
-                      class="spinner-grow spinner-grow-sm me-2"
+                      className="spinner-grow spinner-grow-sm me-2"
                       aria-hidden="true"
                     ></span>
                   )}{" "}
